d74b52646c9d5d6f39600ac07c2730fe19b97c50
alfayedmohammad20-a11y <alfayedmohammad20@gmail.com>
2026-09-09 15:22:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Menambahkan root directory ke Python path agar folder 'backend' bisa di-import
+# Masukkan root directory ke sys.path agar modul 'backend' dapat di-import
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.server import app
