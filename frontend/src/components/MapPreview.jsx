7ad7d15d203b2e7d12b18c209660dc0ae45141e9
alfayedmohammad20-a11y <alfayedmohammad20@gmail.com>
2026-09-11 14:49:58 +0800

    Update MapPreview.jsx

--- frontend/src/components/MapPreview.jsx
+++ frontend/src/components/MapPreview.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import L from "leaflet";
-import http from "../lib/http";
+import http from "../api/http";
 
 function FitBounds({ bounds, map }) {
   useEffect(() => {
