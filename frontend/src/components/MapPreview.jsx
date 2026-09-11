d7b2df25ba1a4b0d3209fd69b5cd34283b21a5be
alfayedmohammad20-a11y <alfayedmohammad20@gmail.com>
2026-09-11 11:20:11 +0800

    Update MapPreview.jsx

--- frontend/src/components/MapPreview.jsx
+++ frontend/src/components/MapPreview.jsx
@@ -136,9 +136,9 @@ export default function MapPreview({ mapId, certificateStatus, areaSize }) {
 
             layer.bindPopup(
               `<div style="font-family:'IBM Plex Sans'"><b>${name}</b><hr/>${content}</div>`
-            );
+              );
           }}
-      />
+          />
       <FitBounds bounds={bounds} map={map} />
     </MapContainer>
   )}
